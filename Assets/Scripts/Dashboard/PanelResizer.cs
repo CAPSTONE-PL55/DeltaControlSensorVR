057fdf4f65046022bfe3ba3826c114e1ab0be983
David Onak <david.onak@shaw.ca>
2023-03-10 23:55:58 -0800

    Added initial panal

---
+++ Assets/Scripts/Dashboard/PanelResizer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelResizer : MonoBehaviour
+{
+    public Button resizeButton;
+    public RectTransform panelRectTransform;
+    public GameObject panelContent;
+    public Text cameraMode;
+
+    private bool isResized = false;
+    private bool isWalkMode = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resizeButton.onClick.AddListener(ResizePanel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            ResizePanel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C)) {
+            if (isWalkMode) {
+                cameraMode.text = "Current Camera Mode: Fly";
+                isWalkMode = false;
+            } else {
+                cameraMode.text = "Current Camera Mode: Walk";
+                isWalkMode = true;
+            }
+        }
+        
+    }
+
+    void ResizePanel()
+    {
+        if (isResized)
+        {
+            resizeButton.GetComponentInChildren<Text>().text = "Hide Dashboard";
+            panelRectTransform.sizeDelta = new Vector2(panelRectTransform.sizeDelta.x, 300f);
+            panelRectTransform.anchoredPosition = new Vector2(panelRectTransform.anchoredPosition.x, -150f);
+            //resizeButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(resizeButton.GetComponent<RectTransform>().anchoredPosition.x, -50f);
+
+            panelContent.SetActive(true);
+            isResized = false;
+        }
+        else
+        {
+            resizeButton.GetComponentInChildren<Text>().text = "Show Dashboard";
+            panelRectTransform.sizeDelta = new Vector2(panelRectTransform.sizeDelta.x, 66f);
+            panelRectTransform.anchoredPosition = new Vector2(panelRectTransform.anchoredPosition.x, -33f);
+            //resizeButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(resizeButton.GetComponent<RectTransform>().anchoredPosition.x, -50f);
+
+            panelContent.SetActive(false);
+            isResized = true;
+        }
+    }
+}
